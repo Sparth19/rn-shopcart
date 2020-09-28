305cc496e2368e5b779e108170f5d71928ed4803
ayushbhaisha <ayushbhaisha@gmail.com>
2020-09-28 19:54:12 +0530

    HomeScreen to PlaceDetail Navigation

--- src/navigation/ShopNavigator.js
+++ src/navigation/ShopNavigator.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
 import CartScreen from '../screens/shop/CartScreen';
-import CategoryProducts from '../screens/shop/CategoryProducts';
+import CategoryProductsScreen from '../screens/shop/CategoryProductsScreen';
 import FavoritesScreen from '../screens/shop/FavoritesScreen';
 import HomeScreen from '../screens/shop/HomeScreen';
 import PlaceOrderScreen from '../screens/shop/PlaceOrderScreen';
@@ -13,10 +13,21 @@ import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
+const HomeNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="CategoryProductsScreen" component={CategoryProductsScreen} />
+      <Stack.Screen name="ProductDetailScreen" component={ProductDetailScreen} />
+    </Stack.Navigator>
+  );
+};
 
 
 const ShopNavigator = (props) => {
-  return <NavigationContainer></NavigationContainer>;
+  return <NavigationContainer>
+    <HomeNavigator />
+  </NavigationContainer>;
 };
 
 export default ShopNavigator;
---
+++ src/screens/shop/FavoritesScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FavoritesScreen = props => {
+    return (
+        <View>
+            <Text>FavoritesScreen
+            
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default FavoritesScreen;
