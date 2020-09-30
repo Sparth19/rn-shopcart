55850ce39c716654ce2d2967973eae703f5fef17
Parth <parthshekhaliya18@gmail.com>
2020-09-30 15:57:17 +0530

    fav screen bux fix

--- src/models/FavoriteItem.js
+++ src/models/FavoriteItem.js
@@ -1,75 +1,71 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    TouchableOpacity,
-    ImageBackground
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 
-const FavoriteItem = props => {
-    return (
-        <View style={styles.favItem}>
-            <TouchableOpacity onPress={props.onSelect}>
-                <View>
-                    <View style={{ ...styles.favRow, ...styles.FavHeader }}>
-                        <ImageBackground
-                            source={{ uri: props.image }}
-                            style={styles.bgImage}
-                        >
-                            <View style={styles.titleContainer}>
-                                <Text style={styles.title} numberOfLines={1}>
-                                    {props.title}
-                                </Text>
-                            </View>
-                        </ImageBackground>
-                    </View>
-                    <View style={{ ...styles.favRow, ...styles.favDetail }}>
-                        <Text>{props.duration}m</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
+const FavoriteItem = (props) => {
+  return (
+    <View style={styles.favItem}>
+      <TouchableOpacity onPress={props.onSelect}>
+        <View>
+          <View style={{...styles.favRow, ...styles.FavHeader}}>
+            <ImageBackground source={{uri: props.image}} style={styles.bgImage}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {props.title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{...styles.favRow, ...styles.favDetail}}>
+            <Text>{props.duration}m</Text>
+          </View>
         </View>
-    );
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    favItem: {
-        height: 200,
-        width: '100%',
-        backgroundColor: '#f5f5f5',
-        borderRadius: 10,
-        overflow: 'hidden',
-        marginVertical: 10
-    },
-    bgImage: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'flex-end',
-    },
-    favRow: {
-        flexDirection: 'row'
-    },
-    FavHeader: {
-        height: '85%'
-    },
-    favDetail: {
-        paddingHorizontal: 10,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '15%'
-    },
-    titleContainer: {
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        paddingVertical: 5,
-        paddingHorizontal: 12
-    },
-    title: {
-        fontFamily: 'open-sans-bold',
-        fontSize: 20,
-        color: 'white',
-        textAlign: 'center'
-    }
+  favItem: {
+    height: 200,
+    width: '100%',
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginVertical: 10,
+  },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  favRow: {
+    flexDirection: 'row',
+  },
+  FavHeader: {
+    height: '85%',
+  },
+  favDetail: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '15%',
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
 });
 
 export default FavoriteItem;
