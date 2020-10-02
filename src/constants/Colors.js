1f88d55a8c77662881aacbca61b35e57c5ffde1b
ayushbhaisha <ayushbhaisha@gmail.com>
2020-10-02 12:09:56 +0530

    ui part one

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,6 +1,16 @@
 export default {
-  primary: '#d9514f',
-  accent: '#2da8de',
+  //primaryColor: '#0294A5',
+  primary: '#C1403D',
+  accent: '#0294A5',
+  background: '#edc6c4'
 };
 //2da8de blue
 //d9514f red
+
+//#0444BF blue type
+//#EDF259 light Yellow
+
+//#0294A5 blue type
+//#C1403D orange type
+//#A53AB3
+//#D50B53 ornage type
